249378275676cecd59e1550df74f61bf753c2ded
Davide Caroselli <davide.caroselli@translated.net>
2016-02-22 17:43:08 +0100

    Bugfix on JFlexTokenizer: added ALL map

--- src/TextProcessing/src/main/java/eu/modernmt/processing/tokenizer/jflex/JFlexTokenizer.java
+++ src/TextProcessing/src/main/java/eu/modernmt/processing/tokenizer/jflex/JFlexTokenizer.java
@@ -1,5 +1,6 @@
 package eu.modernmt.processing.tokenizer.jflex;
 
+import eu.modernmt.processing.Languages;
 import eu.modernmt.processing.framework.ProcessingException;
 import eu.modernmt.processing.tokenizer.MultiInstanceTokenizer;
 import eu.modernmt.processing.tokenizer.Tokenizer;
@@ -9,6 +10,9 @@ import eu.modernmt.processing.tokenizer.jflex.annotators.*;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by davide on 29/01/16.
@@ -36,6 +40,32 @@ public class JFlexTokenizer extends MultiInstanceTokenizer {
     public static final JFlexTokenizer SLOVENE = new JFlexTokenizer(SloveneAnnotator.class);
     public static final JFlexTokenizer SWEDISH = new JFlexTokenizer(SwedishAnnotator.class);
     public static final JFlexTokenizer TAMIL = new JFlexTokenizer(TamilAnnotator.class);
+
+    public static final Map<Locale, Tokenizer> ALL = new HashMap<>();
+
+    static {
+        ALL.put(Languages.CATALAN, CATALAN);
+        ALL.put(Languages.CZECH, CZECH);
+        ALL.put(Languages.GERMAN, GERMAN);
+        ALL.put(Languages.GREEK, GREEK);
+        ALL.put(Languages.ENGLISH, ENGLISH);
+        ALL.put(Languages.SPANISH, SPANISH);
+        ALL.put(Languages.FINNISH, FINNISH);
+        ALL.put(Languages.FRENCH, FRENCH);
+        ALL.put(Languages.HUNGARIAN, HUNGARIAN);
+        ALL.put(Languages.ICELANDIC, ICELANDIC);
+        ALL.put(Languages.ITALIAN, ITALIAN);
+        ALL.put(Languages.LATVIAN, LATVIAN);
+        ALL.put(Languages.DUTCH, DUTCH);
+        ALL.put(Languages.POLISH, POLISH);
+        ALL.put(Languages.PORTUGUESE, PORTUGUESE);
+        ALL.put(Languages.ROMANIAN, ROMANIAN);
+        ALL.put(Languages.RUSSIAN, RUSSIAN);
+        ALL.put(Languages.SLOVAK, SLOVAK);
+        ALL.put(Languages.SLOVENE, SLOVENE);
+        ALL.put(Languages.SWEDISH, SWEDISH);
+        ALL.put(Languages.TAMIL, TAMIL);
+    }
 
     private static class JFlexTokenizerFactory implements TokenizerFactory {
 
