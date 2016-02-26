63e7852e4b90ca065aaf2b945b30cf88f7cec9f4
Luca Mastrostefano <luca.mastrostefano90@gmail.com>
2016-02-26 18:00:03 +0100

    Automatic creation of test.json

--- test/create_new_test.py
+++ test/create_new_test.py
@@ -11,17 +11,31 @@ test_name = sys.argv[1]
 current_path = os.path.dirname(os.path.realpath(__file__))
 tests_path = current_path + os.path.sep + "tests" + os.path.sep + test_name + os.path.sep
 if(os.path.isdir(tests_path)):
-    print "The test specified already exists under \"" + tests_path + "\""
+    print "FAIL: The test specified already exists under \"" + tests_path + "\""
 else:
     os.makedirs(tests_path)
     launch_script_path = tests_path + "launch.sh"
+    content = "#!/bin/sh\n" \
+        "#launch your test here\n" \
+        "\n#Examples:\n" \
+        "#java eu.modernmt.Class \"$@\"\n" \
+        "#python myScript.py \"$@\"\n" \
+        "\n#Use \"$@\" to forward all the arguments to your script."
     with open(launch_script_path,'w') as f:
-        f.write("#!/bin/sh\n"
-                "#launch your test here\n"
-                "\n#Examples:"
-                "\n#java eu.modernmt.Class \"$@\""
-                "\n#python myScript.py \"$@\""
-                "\n\n#Use \"$@\" to forward all the arguments to your script.")
+        f.write(content)
     default_permissions = os.stat(launch_script_path)
     os.chmod(launch_script_path, default_permissions.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-    print "Test structure created under \"" + tests_path + "\""
+
+    json_path = tests_path + "test.json"
+    content = "{\n" \
+        "\t\"enabled\": true,\n" \
+        "\t\"name\": \"" + test_name + "\",\n" \
+        "\t\"description\": \"Description of my the test\"\n" \
+        "}"
+    with open(json_path,'w') as f:
+        f.write(content)
+
+    print "SUCCESS: Test structure created under \"" + tests_path + "\""
+    print "SUCCESS: Create script \"launch.sh\""
+    print "SUCCESS: Create json file \"test.json\""
+    print "\nPlease, edit both \"launch.sh\" and \"test.json\" with the information regarding your new test.\n"
