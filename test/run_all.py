0d8ef03466e655b9ee927fc35f5b022ff8350410
Luca Mastrostefano <luca.mastrostefano90@gmail.com>
2016-02-26 18:00:27 +0100

    Minor changes to the README.md

--- test/run_all.py
+++ test/run_all.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
-
+import os, json
 class Tester:
 
-    def __init__(self):
-        import json
+    TESTS_DIRECTORY_NAME = "tests"
+    JSON_DESCRIPTION_FILE_NAME = "test.json"
 
     def run_all(self):
-        import os
+        print "TODO"
+
+    def list_all_tests(self, verbose):
+        available_test = sorted(self.get_available_tests())
+        if len(available_test) == 0:
+            print "No test found."
+            return
+        for test_dir in available_test:
+            json_file_path = test_dir + os.path.sep + Tester.JSON_DESCRIPTION_FILE_NAME
+            try:
+                json_description = json.load(json_file_path)
+                enabled = True
+                if enabled:
+                    None #Add to enabled
+                else:
+                    None #Add to disabled
+            except ValueError:
+                None #Add to failed
+        print "Available tests:\n"
+
+    def get_available_tests(self):
         current_path = os.path.dirname(os.path.realpath(__file__))
-        tests_path = current_path + os.path.sep + "tests";
-        for f in os.listdir(tests_path):
-            print f
+        tests_path = current_path + os.path.sep + Tester.TESTS_DIRECTORY_NAME
+        return [test for test in os.listdir(tests_path) if os.path.isdir(test)]
 
 if __name__ == "__main__":
-    Tester().run_all()
+    Tester().list_all_tests()
--- test/tests/README.md
+++ test/tests/README.md
@@ -1,9 +1,1 @@
-# Test infrastructure
-
-## Contribute with a new test
-You should create a new directory for each test.
-Inside the directory please create a file named "launch.sh"
-
-```
-eu.modernmt.test.{name_of_module_to_test}.{package_of_the_class_to_test}
-```
+Please read [TEST.md](../TEST.md) to know how to add a new test or how to launch an existing one.
