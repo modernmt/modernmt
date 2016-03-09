a28a7de4546954b42733c4b0c309d87ea75531f0
Ubuntu <ubuntu@ip-172-31-44-75.ec2.internal>
2016-03-09 10:04:02 +0000

    Created Aligner module

---
+++ src/Aligner/src/main/java/eu/modernmt/Aligner.java
@@ -0,0 +1,13 @@
+package eu.modernmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class Aligner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
