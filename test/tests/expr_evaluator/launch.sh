723ff88f9a26842423b2e9fb868d29ce97b8ca2a
cattoni <cattoni@fbk.eu>
2016-03-11 14:57:23 +0000

    added initial dir expr_evaluator

---
+++ test/tests/expr_evaluator/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Launch your test here
+
+#Examples:
+#java eu.modernmt.expr_evaluator "$@"
+#python expr_evaluator.py "$@"
+#./expr_evaluator.sh "$@"
+
+#Note: use "$@" to forward all the arguments to your script.
