1d10ee2c3a4c061ad696fa510945ecd8f53c02e4
Davide Caroselli <davide.caroselli@translated.net>
2016-03-25 14:14:26 +0100

    Added MultiplexedLM training

--- scripts/mt/lm.py
+++ scripts/mt/lm.py
@@ -20,10 +20,8 @@ class LanguageModel(MosesFeature):
     def instantiate(type_name, *model):
         if type_name == 'KenLM':
             return KenLM(*model)
-        elif type_name == 'AdaptiveIRSTLM':
-            return AdaptiveIRSTLM(*model)
-        elif type_name == 'MultiplexedIRSTLM':
-            return MultiplexedIRSTLM(*model)
+        elif type_name == 'MultiplexedLM':
+            return MultiplexedLM(*model)
         elif type_name == 'StaticIRSTLM':
             return StaticIRSTLM(*model)
         else:
@@ -51,6 +49,7 @@ class KenLM(LanguageModel):
     def __init__(self, model):
         LanguageModel.__init__(self, model, 'KENLM')
 
+        self.prune = True
         self._lmplz_bin = os.path.join(scripts.BIN_DIR, 'kenlm-stable', 'bin', 'lmplz')
         self._bbinary_bin = os.path.join(scripts.BIN_DIR, 'kenlm-stable', 'bin', 'build_binary')
 
@@ -69,7 +68,10 @@ class KenLM(LanguageModel):
 
             # Create language model in ARPA format
             arpa_file = os.path.join(working_dir, 'lm.arpa')
-            arpa_command = [self._lmplz_bin, '--discount_fallback', '--prune', '0', '0', '1', '-o', str(self._order)]
+            arpa_command = [self._lmplz_bin, '--discount_fallback', '-o', str(self._order)]
+            if self.prune:
+                arpa_command += ['--prune', '0', '0', '1']
+
             with open(merged_corpus) as stdin:
                 with open(arpa_file, 'w') as stdout:
                     shell.execute(arpa_command, stdin=stdin, stdout=stdout, stderr=log)
@@ -135,98 +137,33 @@ class StaticIRSTLM(LanguageModel):
         return 'factor=0 path={model} dub=10000000'.format(model=self.get_relpath(self._model))
 
 
-class AdaptiveIRSTLM(LanguageModel):
-    injector_section = 'adaptivelm'
-    injectable_fields = {
-        'order': ('LM order (N-grams length)', int, 2),
-        'limit': ('Limit the LMs to be queried at runtime', int, 1),
-    }
-
-    def __init__(self, model):
-        LanguageModel.__init__(self, model, 'IRSTLM')
-
-        self._limit = None  # Injected
-
-        self._model_dir = os.path.abspath(os.path.join(model, os.pardir))
-
-        self._irstlm_dir = os.path.join(scripts.BIN_DIR, 'irstlm-adaptivelm-v0.6')
-        self._addbound_bin = os.path.join(self._irstlm_dir, 'scripts', 'add-start-end.sh')
-        self._buildlm_bin = os.path.join(self._irstlm_dir, 'scripts', 'build-lm.sh')
-        self._compilelm_bin = os.path.join(self._irstlm_dir, 'bin', 'compile-lm')
-
-    def train(self, corpora, lang, working_dir='.', log_file=None):
-        LanguageModel.train(self, corpora, lang, working_dir, log_file)
-
-        log = shell.DEVNULL
-
-        try:
-            if log_file is not None:
-                log = open(log_file, 'w')
-
-            lmconfig_content = ['LMINTERPOLATION ' + str(len(corpora)) + ' MAP']
-            w = 1. / len(corpora)
-
-            models_folder = os.path.dirname(self._model)
-
-            for corpus in corpora:
-                cfile = corpus.get_file(lang)
-                lm = corpus.name + '.alm'
-
-                lmconfig_content.append('{weight} {name} {lm}'.format(weight=str(w), name=corpus.name, lm=lm))
-
-                self._train_lm(cfile, os.path.join(models_folder, lm), working_dir, log)
-
-            with open(self._model, 'w') as model:
-                for line in lmconfig_content:
-                    model.write(line)
-                    model.write('\n')
-        finally:
-            if log_file is not None:
-                log.close()
-
-    def _train_lm(self, source, dest, working_dir, log):
-        input_se = os.path.join(working_dir, 'input.se')
-        temp = os.path.join(working_dir, 'temp')
-        arpa_file = os.path.join(working_dir, 'arpa')
-
-        # Add start and end symbols
-        with open(source) as stdin:
-            with open(input_se, 'w') as stdout:
-                shell.execute([self._addbound_bin], stdin=stdin, stdout=stdout, stderr=log)
-
-        # Creating lm in ARPA format
-        command = [self._buildlm_bin, '-i', input_se, '-k', str(cpu_count()), '-o', arpa_file, '-n', str(self._order),
-                   '-s', 'witten-bell', '-t', temp, '-l', '/dev/stdout', '-irstlm', self._irstlm_dir]
-        shell.execute(command, stderr=log)
-
-        # Create binary lm
-        command = [self._compilelm_bin, arpa_file + '.gz', dest]
-        shell.execute(command, stderr=log)
-
-    def get_iniline(self):
-        return 'factor=0 path={model} dub=10000000 weight_normalization=yes weight_limit={limit}'.format(
-            model=self.get_relpath(self._model), limit=self._limit)
-
-
-class MultiplexedIRSTLM(LanguageModel):
+class MultiplexedLM(LanguageModel):
     injector_section = 'muxlm'
     injectable_fields = {
         'order': ('LM order (N-grams length)', int, 5),
         'alpha': ('Adaptive LM weight fraction from [0,1), the rest is assigned to background LM', float, 0.5),
-        'function': ('interpolation function', str, 'interpolate-linear'),
+        'function': ('interpolation function',
+                     (basestring, ['interpolate-linear', 'interpolate-log-linear', 'interpolate-max']),
+                     'interpolate-linear'),
     }
 
     def __init__(self, model):
         LanguageModel.__init__(self, model, 'MUXLM')
 
-        # Injected
-        self._function = None
-        self._alpha = None
+        self._function = None  # Injected
+        self._alpha = None  # Injected
+
+    def __get_config_line(self, name, model):
+        return 'KENLM name={name} factor=0 order={order} path={model}'.format(name=name, model='${LM_PATH}' + model,
+                                                                              order=self._order)
 
     def train(self, corpora, lang, working_dir='.', log_file=None):
         LanguageModel.train(self, corpora, lang, working_dir, log_file)
 
-
+        bicorpora = []
+        for corpus in corpora:
+            if len(corpus.langs) > 1:
+                bicorpora.append(corpus)
 
         log = shell.DEVNULL
 
@@ -234,36 +171,32 @@ class MultiplexedIRSTLM(LanguageModel):
             if log_file is not None:
                 log = open(log_file, 'w')
 
-            lmconfig_content = ['[muxlm]']
-            w = 1. / len(corpora)
+            model_folder = os.path.dirname(self._model)
+            config_content = ['[muxlm]']
 
-            models_folder = os.path.dirname(self._model)
+            # Train static LM
+            static_lm_model = os.path.join(model_folder, 'background.slm')
+            static_lm = KenLM(static_lm_model)
+            static_lm._order = self._order
+            static_lm.train(corpora, lang, os.path.join(working_dir, 'background.slm'))
+            config_content.append(self.__get_config_line('__background_lm__', 'background.slm'))
 
-            # also need background LM here.
-            # TODO: rename member _model -> model? (we are accessing private-marked members here)
-            bglm_model = self._static_lm._model
-            bglm_order = self._static_lm._order
-            lmconfig_content.append('KENLM name=BackgroundLM factor=0 order={order} path={path}'.format(order=bglm_order, path=bglm_model))
-
-            for corpus in corpora:
-                cfile = corpus.get_file(lang)
-                lm = corpus.name + '.alm'
-                dest = os.path.join(models_folder, lm)
-
-                lmconfig_content.append('IRSTLM name={name} factor=0 order={order} path={dest}'.format(name=corpus.name, order=self._order, dest=dest))
-
-                self._train_lm(cfile, dest, working_dir, log)
+            # Train domain-specific LMs
+            for corpus in bicorpora:
+                adaptive_lm_model = os.path.join(model_folder, corpus.name + '.alm')
+                adaptive_lm = KenLM(adaptive_lm_model)
+                adaptive_lm.prune = False
+                adaptive_lm._order = self._order
+                adaptive_lm.train([corpus], lang, os.path.join(working_dir, corpus.name + '.alm'))
+                config_content.append(self.__get_config_line(corpus.name, corpus.name + '.alm'))
 
             with open(self._model, 'w') as model:
-                for line in lmconfig_content:
-                    model.write(line)
-                    model.write('\n')
+                model.write('\n'.join(config_content))
+                model.write('\n')
         finally:
             if log_file is not None:
                 log.close()
 
-    # def _train_lm() inherited
-
     def get_iniline(self):
-        return 'factor=0 path={model} background-lm=BackgroundLM alpha={alpha} function={function}'.format(
+        return 'factor=0 path={model} background-lm=__background_lm__ alpha={alpha} function={function}'.format(
             model=self.get_relpath(self._model), alpha=self._alpha, function=self._function)
