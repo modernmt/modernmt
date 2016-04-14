850aa39146b12d4a1c3bb5306d387548a4a96283
Luca Mastrostefano <luca.mastrostefano90@gmail.com>
2016-04-14 17:48:01 +0200

    Bug fix: indexes were wrongly scanned

--- src/Aligner/src/main/java/eu/modernmt/aligner/fastalign/FastAlign.java
+++ src/Aligner/src/main/java/eu/modernmt/aligner/fastalign/FastAlign.java
@@ -12,9 +12,7 @@ import org.apache.logging.log4j.Logger;
 import javax.naming.OperationNotSupportedException;
 import java.io.*;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by lucamastrostefano on 15/03/16.
@@ -140,13 +138,25 @@ public class FastAlign implements Aligner, Closeable {
     }
 
     public static int[][] interpolateAlignments(int[][] alignments, int numberOfSourceTokens, int numberOfTargetTokens) {
+        alignments = Arrays.copyOf(alignments, alignments.length + 1);
+        alignments[alignments.length - 1] = new int[]{numberOfSourceTokens, numberOfTargetTokens};
         ArrayList<int[]> interpolatedAlignments = new ArrayList<>(alignments.length);
+        Arrays.sort(alignments, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a1, int[] a2) {
+                int c = a1[0] - a2[0];
+                if (c == 0) {
+                    c = a1[1] - a2[1];
+                }
+                return c;
+            }
+        });
         BitSet targetCoveredTokens = new BitSet(numberOfTargetTokens);
         for (int[] alignment : alignments) {
             targetCoveredTokens.set(alignment[1]);
         }
-        int prevSourceIndex = 0;
-        int prevTargetIndex = 0;
+        int prevSourceIndex = -1;
+        int prevTargetIndex = -1;
         for (int alignmentIndex = 0; alignmentIndex < alignments.length; alignmentIndex++) {
             int[] alignment = alignments[alignmentIndex];
             int sourceIndex = alignment[0];
@@ -154,22 +164,22 @@ public class FastAlign implements Aligner, Closeable {
             int sourceDiff = sourceIndex - prevSourceIndex;
             int targetDiff = targetIndex - prevTargetIndex;
             if (sourceDiff > 1 || Math.abs(targetDiff) > 1) {
+                int minTarget = Math.min(prevTargetIndex, targetIndex);
+                int maxTarget = Math.max(prevTargetIndex, targetIndex);
                 boolean monotoneBlock = true;
-                for (int t = prevTargetIndex + 1; t < targetIndex; t++) {
+                for (int t = minTarget + 1; t < maxTarget; t++) {
                     if (targetCoveredTokens.get(t)) {
                         monotoneBlock = false;
                         break;
                     }
                 }
                 if (monotoneBlock) {
-                    int minTarget = Math.min(prevTargetIndex, targetIndex);
-                    int maxTarget = Math.max(prevTargetIndex, targetIndex);
                     if (sourceDiff == 0) {
-                        for (int t = minTarget; t < maxTarget; t++) {
+                        for (int t = minTarget + 1; t < maxTarget; t++) {
                             interpolatedAlignments.add(new int[]{sourceIndex, t});
                         }
                     } else if (targetDiff == 0) {
-                        for (int s = prevSourceIndex; s <= sourceIndex; s++) {
+                        for (int s = prevSourceIndex + 1; s < sourceIndex; s++) {
                             interpolatedAlignments.add(new int[]{s, targetIndex});
                         }
                     } else {
