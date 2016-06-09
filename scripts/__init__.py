bab7eaae96825eaeb527b05ecb481604647bcac6
Davide Caroselli <davide.caroselli@translated.net>
2016-06-09 17:57:26 +0200

    Update MMT version to 0.13

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Davide Caroselli'
 
 __self_dir = os.path.dirname(os.path.realpath(__file__))
 
-MMT_VERSION = '0.12'
+MMT_VERSION = '0.13'
 
 MMT_ROOT = os.path.abspath(os.path.join(__self_dir, os.pardir))
 ENGINES_DIR = os.path.join(MMT_ROOT, 'engines')
