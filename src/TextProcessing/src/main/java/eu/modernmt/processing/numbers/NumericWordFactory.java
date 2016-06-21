3ae98a49742a01c208ae3bbb907d324cad6ef3ac
Davide Caroselli <davide.caroselli@translated.net>
2016-06-21 10:51:06 +0200

    Numerical expressions are now replaces with all 0s instead of sequence of 1, 2, ...

--- src/TextProcessing/src/main/java/eu/modernmt/processing/numbers/NumericWordFactory.java
+++ src/TextProcessing/src/main/java/eu/modernmt/processing/numbers/NumericWordFactory.java
@@ -4,7 +4,6 @@ import eu.modernmt.model.Word;
 import eu.modernmt.processing.SentenceBuilder;
 import eu.modernmt.processing.WordTransformationFactory;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -15,16 +14,6 @@ public class NumericWordFactory implements SentenceBuilder.WordFactory, WordTran
 
     private static final Pattern PATTERN = Pattern.compile(".*[0-9].*");
     private static final NumericWordTransformation TRANSFORMATION = new NumericWordTransformation();
-
-    private static class Counter {
-        public int value;
-
-        Counter(int value) {
-            this.value = value;
-        }
-    }
-
-    private HashMap<String, Counter> pattern2Count = new HashMap<>();
 
     // WordTransformationFactory.WordTransformer
 
@@ -51,19 +40,8 @@ public class NumericWordFactory implements SentenceBuilder.WordFactory, WordTran
         char[] chars = placeholder.toCharArray();
 
         replaceDigits(chars, 0);
-        placeholder = new String(chars);
 
-        Counter count = pattern2Count.get(placeholder);
-        if (count == null) {
-            pattern2Count.put(placeholder, new Counter(1));
-        } else {
-            replaceDigits(chars, count.value);
-            placeholder = new String(chars);
-
-            count.value++;
-        }
-
-        Word word = new Word(text, placeholder, rightSpace);
+        Word word = new Word(text, new String(chars), rightSpace);
         word.setTransformation(TRANSFORMATION);
 
         return word;
