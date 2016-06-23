19fdc0b3e6e7ce6cdf308bb3c9addbfaf1cc7e87
Davide Caroselli <davide.caroselli@translated.net>
2016-06-23 10:40:57 +0200

    Replaced compile.sh with more standard Makefile. New build/ structure.

--- src/Moses/native/cmake/FindMoses.cmake
+++ src/Moses/native/cmake/FindMoses.cmake
@@ -4,20 +4,19 @@
 #  Moses_INCLUDE_DIRS
 #  Moses_LIBRARIES
 
-# MMT builds mosesdecoder in <MMT>/vendor/moses and install prefix is <MMT>, so
-# we expect Moses headers to be in <MMT>/include
+# MMT builds mosesdecoder in <MMT>/vendor/moses and install prefix is <MMT>/build
 #
 # can either use CMAKE_SOURCE_DIR which will be <MMT> if built on that level, and
-#   <MMT>/src/Decoder/native if only Decoder is built.
+#   <MMT>/src/Moses/native if only Decoder is built.
 #
-# or, we can use PROJECT_SOURCE_DIR which should always point to <MMT>/src/Decoder/native
+# or, we can use PROJECT_SOURCE_DIR which should always point to <MMT>/src/Moses/native
 #   for the Decoder project.
 #
 find_path(Moses_INCLUDE_DIRS moses/FF/StatefulFeatureFunction.h
           HINTS "${PROJECT_SOURCE_DIR}/../../../vendor/moses/")
 
 find_library(Moses_LIBRARIES NAMES moses libmoses
-             HINTS "${PROJECT_SOURCE_DIR}/../../../lib/")
+             HINTS "${PROJECT_SOURCE_DIR}/../../../build/lib/")
 
 # find_path(Moses_LIBRARIES build/lib/${CMAKE_SHARED_LIBRARY_PREFIX}moses${CMAKE_SHARED_LIBRARY_SUFFIX} PATHS ENV MosesDECODER_HOME)
 #mark_as_advanced(LIBXML2_INCLUDE_DIR LIBXML2_LIBRARY )
