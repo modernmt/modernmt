211b3756bf85a481ee92f4482a17900e9ff08ce2
Davide Caroselli <davide.caroselli@translated.net>
2016-06-23 10:17:04 +0200

    Removed IRSTLM dependency

--- vendor/compile.sh
+++ vendor/compile.sh
@@ -19,16 +19,6 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MMT_HOME="$DIR/../"
 
-# Build IRSTLM
-mkdir -p "$DIR/irstlm/build"
-pushd "$DIR/irstlm/build" &> /dev/null
-
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$MMT_HOME" -DCXX0:BOOL=OFF -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF
-make -j$(nproc)
-make install
-
-popd &> /dev/null
-
 # Build Moses
 mkdir -p "$DIR/moses/build"
 pushd "$DIR/moses/build" &> /dev/null
