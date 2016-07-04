a333ae17add5d76aa10fa96e90f2e2078ff8d694
Davide Caroselli <davide.caroselli@translated.net>
2016-07-04 19:47:31 +0200

    Added common XMl utilities class: XMLUtils

--- src/Model/src/main/java/eu/modernmt/model/impl/fourcb/FourCBFileReader.java
+++ src/Model/src/main/java/eu/modernmt/model/impl/fourcb/FourCBFileReader.java
@@ -2,17 +2,20 @@ package eu.modernmt.model.impl.fourcb;
 
 import eu.modernmt.constants.Const;
 import eu.modernmt.io.LineReader;
+import eu.modernmt.xml.XMLUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.BOMInputStream;
 
-import javax.xml.XMLConstants;
-import javax.xml.namespace.QName;
-import javax.xml.stream.*;
-import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.EndElement;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by davide on 04/07/16.
@@ -53,7 +56,7 @@ class FourCBFileReader implements LineReader {
                 switch (event.getEventType()) {
                     case XMLStreamConstants.START_ELEMENT:
                         StartElement element = event.asStartElement();
-                        String name = getLocalName(element);
+                        String name = XMLUtils.getLocalName(element);
 
                         if ("ContentElement".equals(name)) {
                             return readContentElement(reader, element);
@@ -70,46 +73,16 @@ class FourCBFileReader implements LineReader {
     }
 
     private Line4CB readContentElement(XMLEventReader reader, StartElement element) throws XMLStreamException {
-        String id = getAttributeValue(element, null, "id");
-        StringWriter buffer = new StringWriter(1024);
+        String id = XMLUtils.getAttributeValue(element, null, "id");
+        String line = XMLUtils.getXMLContent(reader, element, false);
 
-        StringWriter lastXMLTagWriter = new StringWriter(128);
-        String pendingElementName = null;
-
-        while (reader.hasNext()) {
-            XMLEvent event = reader.nextEvent();
-
-            if (event.isEndElement() && "ContentElement".equals(getLocalName(event.asEndElement()))) {
-                return new Line4CB(buffer.toString(), id);
-            }
-
-            if (pendingElementName != null) {
-                String tag = lastXMLTagWriter.toString();
-                lastXMLTagWriter.getBuffer().setLength(0);
-
-                if (event.isEndElement() && pendingElementName.equals(getLocalName(event.asEndElement()))) {
-                    tag = tag.substring(0, tag.length() - 1) + "/>";
-                    buffer.append(tag);
-                    continue;
-                } else {
-                    buffer.append(tag);
-                }
-            }
-
-            if (event.isStartElement()) {
-                event.writeAsEncodedUnicode(lastXMLTagWriter);
-                pendingElementName = getLocalName(event.asStartElement());
-            } else {
-                event.writeAsEncodedUnicode(buffer);
-            }
-        }
-
-        throw new XMLStreamException(format("Missing closing tag for 'ContentElement' element", element));
+        return new Line4CB(id, line);
     }
 
     @Override
     public String readLine() throws IOException {
-        return readLineWithMetadata().line;
+        Line4CB mline = readLineWithMetadata();
+        return mline == null ? null : mline.line;
     }
 
     @Override
@@ -127,30 +100,10 @@ class FourCBFileReader implements LineReader {
         public final String line;
         public final String id;
 
-        public Line4CB(String line, String id) {
+        private Line4CB(String id, String line) {
             this.line = line;
             this.id = id;
         }
     }
 
-    // Utils
-
-    private static final String format(String message, XMLEvent event) {
-        Location location = event == null ? null : event.getLocation();
-        return location == null ? message : (message + " at line " + location.getLineNumber());
-    }
-
-    private static final String getLocalName(StartElement element) {
-        return element.getName().getLocalPart();
-    }
-
-    private static final String getLocalName(EndElement element) {
-        return element.getName().getLocalPart();
-    }
-
-    private static final String getAttributeValue(StartElement element, String namespaceURI, String localPart) {
-        QName name = new QName(namespaceURI == null ? XMLConstants.NULL_NS_URI : namespaceURI, localPart);
-        Attribute attribute = element.getAttributeByName(name);
-        return attribute == null ? null : attribute.getValue();
-    }
 }
---
+++ src/Model/src/main/java/eu/modernmt/xml/XMLUtils.java
@@ -0,0 +1,141 @@
+package eu.modernmt.xml;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+import javax.xml.stream.Location;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.*;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Iterator;
+
+/**
+ * Created by davide on 04/07/16.
+ */
+public class XMLUtils {
+
+    public static String getLocalName(StartElement element) {
+        return element.getName().getLocalPart();
+    }
+
+    public static String getLocalName(EndElement element) {
+        return element.getName().getLocalPart();
+    }
+
+    public static String getAttributeValue(StartElement element, String namespaceURI, String localPart) {
+        QName name = new QName(namespaceURI == null ? XMLConstants.NULL_NS_URI : namespaceURI, localPart);
+        Attribute attribute = element.getAttributeByName(name);
+        return attribute == null ? null : attribute.getValue();
+    }
+
+    public static String getXMLContent(XMLEventReader reader, StartElement element, boolean decodeCharacters) throws XMLStreamException {
+        String rootElementName = getLocalName(element);
+
+        StringWriter buffer = new StringWriter(1024);
+
+        StartElement pendingElement = null;
+        String pendingElementName = null;
+
+        while (reader.hasNext()) {
+            XMLEvent event = reader.nextEvent();
+
+            if (pendingElement != null) {
+                boolean skip = false;
+
+                if (event.isEndElement() && pendingElementName.equals(getLocalName(event.asEndElement()))) {
+                    writeAsEncodedUnicode(pendingElement, buffer, true); // empty tag
+                    skip = true; // skip this end tag
+                } else {
+                    writeAsEncodedUnicode(pendingElement, buffer, false);
+                }
+
+                pendingElement = null;
+                pendingElementName = null;
+
+                if (skip)
+                    continue;
+            }
+
+            if (event.isEndElement()) {
+                EndElement endElement = event.asEndElement();
+                String name = getLocalName(endElement);
+
+                if (rootElementName.equals(name))
+                    return buffer.toString();
+
+                writeAsEncodedUnicode(endElement, buffer);
+            } else if (event.isStartElement()) {
+                pendingElement = event.asStartElement();
+                pendingElementName = getLocalName(pendingElement);
+            } else if (event.isCharacters() && decodeCharacters) {
+                buffer.append(event.asCharacters().getData());
+            } else {
+                event.writeAsEncodedUnicode(buffer);
+            }
+        }
+
+        throw new XMLStreamException(format("Missing closing tag for '" + rootElementName + "' element", element));
+    }
+
+    private static void writeAsEncodedUnicode(StartElement element, Writer writer, boolean isEmpty) throws XMLStreamException {
+        try {
+            // Write start tag.
+            writer.write('<');
+            QName name = element.getName();
+
+            String prefix = name.getPrefix();
+            if (prefix != null && prefix.length() > 0) {
+                writer.write(prefix);
+                writer.write(':');
+            }
+            writer.write(name.getLocalPart());
+
+            // Write namespace declarations.
+            Iterator nsIter = element.getNamespaces();
+            while (nsIter.hasNext()) {
+                Namespace ns = (Namespace) nsIter.next();
+                writer.write(' ');
+                ns.writeAsEncodedUnicode(writer);
+            }
+
+            // Write attributes
+            Iterator attrIter = element.getAttributes();
+            while (attrIter.hasNext()) {
+                Attribute attr = (Attribute) attrIter.next();
+                writer.write(' ');
+                attr.writeAsEncodedUnicode(writer);
+            }
+
+            if (isEmpty)
+                writer.write('/');
+            writer.write('>');
+        } catch (IOException ioe) {
+            throw new XMLStreamException(ioe);
+        }
+    }
+
+    private static void writeAsEncodedUnicode(EndElement element, Writer writer) throws XMLStreamException {
+        try {
+            // Write end tags.
+            writer.write("</");
+            QName name = element.getName();
+            String prefix = name.getPrefix();
+            if (prefix != null && prefix.length() > 0) {
+                writer.write(prefix);
+                writer.write(':');
+            }
+            writer.write(name.getLocalPart());
+            writer.write('>');
+        } catch (IOException ioe) {
+            throw new XMLStreamException(ioe);
+        }
+    }
+
+    private static String format(String message, XMLEvent event) {
+        Location location = event == null ? null : event.getLocation();
+        return location == null ? message : (message + " at line " + location.getLineNumber());
+    }
+
+}
