b8a6e1fa19b17712b6b7ed62238e3ccd07a0a60c
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2016-09-28 18:43:48 +0200

    changes to FindRocksdb

--- src/suffixarray-phrasetable/native/cmake_modules/FindRocksdb.cmake
+++ src/suffixarray-phrasetable/native/cmake_modules/FindRocksdb.cmake
@@ -5,14 +5,14 @@
 #  Rocksdb_FOUND, whether rocksdb has been found
 
 set(Rocksdb_SEARCH_HEADER_PATHS
-        ${ROCKSDB_ROOT} $ENV{ROCKSDB_ROOT}
+        ${ROCKSDB_ROOT}/include $ENV{ROCKSDB_ROOT}/include
         )
 
 set(Rocksdb_SEARCH_LIB_PATH
-        ${ROCKSDB_ROOT} $ENV{ROCKSDB_ROOT}
+        ${ROCKSDB_ROOT}/lib $ENV{ROCKSDB_ROOT}/lib
         )
 
-find_path(Rocksdb_INCLUDE_DIR include/rocksdb/db.h PATHS
+find_path(Rocksdb_INCLUDE_DIR rocksdb/db.h PATHS
         ${Rocksdb_SEARCH_HEADER_PATHS}
         )
 
@@ -21,14 +21,13 @@ find_library(Rocksdb_LIB_PATH NAMES rocksdb PATHS ${Rocksdb_SEARCH_LIB_PATH})
 if (Rocksdb_INCLUDE_DIR AND Rocksdb_LIB_PATH)
     set(Rocksdb_FOUND TRUE)
     set(Rocksdb_LIBS ${Rocksdb_LIB_PATH})
-    set(Rocksdb_INCLUDE_DIR ${Rocksdb_INCLUDE_DIR}/include)
 else ()
     set(Rocksdb_FOUND FALSE)
 endif ()
 
 if (Rocksdb_FOUND)
     if (NOT Rocksdb_FIND_QUIETLY)
-        message(STATUS "Found the Rocksdb library: ${Rocksdb_LIB_PATH}")
+        message(STATUS "Found the Rocksdb library: ${Rocksdb_LIB_PATH} ${Rocksdb_INCLUDE_DIR}")
     endif ()
 else ()
     if (NOT Rocksdb_FIND_QUIETLY)
