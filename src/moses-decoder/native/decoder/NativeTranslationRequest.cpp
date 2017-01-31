fe907ec82ac6452be57bcad412c8e53c426fa3c5
David Madl <git@abanbytes.eu>
2017-01-31 22:20:08 +0100

    fixup: don't access m_source. assert is in Manager::Decode() already

--- src/moses-decoder/native/decoder/NativeTranslationRequest.cpp
+++ src/moses-decoder/native/decoder/NativeTranslationRequest.cpp
@@ -43,14 +43,15 @@ NativeTranslationRequest(translation_request_t const& paramList) :
 {
 }
 
-static void Translate(translation_request_t const& request, Session const& S, translation_t &result) {
-  boost::shared_ptr<ContextScope> scope = S.scope;
+static void Translate(translation_request_t const& request, boost::shared_ptr<ContextScope> scope, translation_t &result) {
 
+  // TODO move
   // settings within the *sentence* scope - SetContextWeights() can only override the empty ContextScope if we are not within a session
   if(request.contextWeights.size() > 0) {
     boost::shared_ptr<std::map<std::string,float> > M(new std::map<std::string, float>(request.contextWeights));
     scope->SetContextWeights(M);
   }
+  // end TODO
 
   boost::shared_ptr<Moses::AllOptions> opts(new Moses::AllOptions());
   *opts = *StaticData::Instance().options();
@@ -78,20 +79,11 @@ void
 NativeTranslationRequest::
 Run()
 {
-  //parse_request();
-  // cerr << "SESSION ID" << ret->m_session_id << endl;
-
-
-  if(Moses::is_syntax(m_source->options()->search.algo))
-    UTIL_THROW2("syntax-based decoding is not supported in MMT decoder");
-
-  //run_phrase_decoder();
-
   Session const& S = m_translator->get_session(m_session_id);
   m_scope = S.scope;
   m_session_id = S.id;
 
-  Translate(m_paramList, S, m_retData);
+  Translate(m_paramList, S.scope, m_retData);
 }
 
 } // namespace MosesServer
--- src/moses-decoder/native/decoder/Translator.h
+++ src/moses-decoder/native/decoder/Translator.h
@@ -20,42 +20,9 @@ class ThreadPool;
 
 namespace MosesServer {
 
-/**
- * Hypothesis used in n-best list response.
- */
-struct ResponseHypothesis {
-  std::string text; //< result target sentence
-  float score;
-  std::string fvals;
-};
-
-/**
- * Decoder response to a translation request of a sentence.
- */
-struct TranslationResponse {
-  std::string text; //< result target sentence
-  int64_t session; //< resulting session ID
-  std::vector<hypothesis_t> hypotheses; //< n-best list, only filled if requested
-  std::vector<std::pair<size_t, size_t> > alignment; //< word alignment using test-trg pairs, e.g. [(0,1), (1,0), ...]
-};
-
-/**
- * Translation request of a sentence.
- */
-struct TranslationRequest {
-  std::string sourceSent;
-  size_t nBestListSize; //< set to 0 if no n-best list requested
-  uint64_t sessionId; //< 0 means none, 1 means new
-  std::map<std::string, float> contextWeights; //< maps from subcorpus name to weight
-};
-
 class SessionCache;
 class Session;
 
-/**
- * Binary interface to moses decoder. Before usage, load a moses.ini file
- * via Moses::StaticData::LoadDataStatic(), including feature functions.
- */
 class
 Translator
 {
