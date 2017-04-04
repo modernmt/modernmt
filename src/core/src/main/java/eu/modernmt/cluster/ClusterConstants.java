1aaa7d78a8a625a76a476203e8490b5fb27c2b42
Davide Caroselli <davide.caroselli@translated.net>
2017-04-04 12:25:19 +0200

    Removed unused constants

--- src/core/src/main/java/eu/modernmt/cluster/ClusterConstants.java
+++ src/core/src/main/java/eu/modernmt/cluster/ClusterConstants.java
@@ -6,8 +6,6 @@ package eu.modernmt.cluster;
 public class ClusterConstants {
 
     public static final String TRANSLATION_EXECUTOR_NAME = "TranslationsExecutor";
-    public static final String TRANSLATION_SESSION_MAP_NAME = "TranslationSessionMap";
-    public static final String TRANSLATION_SESSION_ID_GENERATOR_NAME = "TranslationSessionIdGenerator";
     public static final String DECODER_WEIGHTS_TOPIC_NAME = "DecoderWeightsTopic";
 
 }
