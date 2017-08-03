bfef7a275e76c49f419edc5126682790db28604f
Davide Caroselli <davide.caroselli@translated.net>
2017-08-03 10:47:05 +0200

    Implemented Korean tokenizer

---
+++ src/textprocessing/src/main/java/eu/modernmt/processing/tokenizer/jflex/ChineseJFlexTokenizer.java
@@ -0,0 +1,25 @@
+package eu.modernmt.processing.tokenizer.jflex;
+
+import eu.modernmt.model.Languages;
+import eu.modernmt.processing.LanguageNotSupportedException;
+import eu.modernmt.processing.string.SentenceBuilder;
+
+import java.util.Locale;
+
+/**
+ * Created by davide on 03/08/17.
+ */
+public class ChineseJFlexTokenizer extends JFlexTokenizer {
+
+    public ChineseJFlexTokenizer(Locale sourceLanguage, Locale targetLanguage) throws LanguageNotSupportedException {
+        super(sourceLanguage, targetLanguage);
+
+        if (!Languages.sameLanguage(Locale.CHINESE, sourceLanguage))
+            throw new LanguageNotSupportedException(sourceLanguage, targetLanguage);
+    }
+
+    @Override
+    protected TokensAnnotatedString wrap(SentenceBuilder builder) {
+        return new TokensAnnotatedString(builder.toString(), true);
+    }
+}
