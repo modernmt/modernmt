e3efe490a67f3cd990e7650d5e4ebf5dc203946a
Davide Caroselli <davide.caroselli@translated.net>
2017-11-24 18:46:21 +0100

    Fixed bug that prevented Priority to be read

---
+++ src/core/src/main/java/eu/modernmt/cluster/services/PriorityFuture.java
@@ -0,0 +1,49 @@
+package eu.modernmt.cluster.services;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by davide on 24/11/17.
+ */
+class PriorityFuture<T> implements RunnableFuture<T>, Prioritizable {
+
+    private RunnableFuture<T> src;
+    private int priority;
+
+    public PriorityFuture(RunnableFuture<T> other, int priority) {
+        this.src = other;
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return src.cancel(mayInterruptIfRunning);
+    }
+
+    public boolean isCancelled() {
+        return src.isCancelled();
+    }
+
+    public boolean isDone() {
+        return src.isDone();
+    }
+
+    public T get() throws InterruptedException, ExecutionException {
+        return src.get();
+    }
+
+    public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return src.get(timeout, unit);
+    }
+
+    public void run() {
+        src.run();
+    }
+
+}
--- src/core/src/main/java/eu/modernmt/cluster/services/TranslationService.java
+++ src/core/src/main/java/eu/modernmt/cluster/services/TranslationService.java
@@ -12,13 +12,14 @@ import java.util.concurrent.*;
  * A TranslationService is an Hazelcast Service for performing translations in a ModernMT cluster.
  * It allows cluster members  to ask to other cluster members to perform translations;
  * translations, moreover, will be run taking their priority into account.
- *
+ * <p>
  * As always in Hazelcast Services, this service is reached by cluster members through local proxies.
- * @see TranslationServiceProxy
  *
+ * @see TranslationServiceProxy
+ * <p>
  * This TranslationService is typically initialized at cluster start.
  */
-public class TranslationService  implements ManagedService, RemoteService {
+public class TranslationService implements ManagedService, RemoteService {
 
     public static final String SERVICE_NAME = "mmt:cluster:TranslationService";
 
@@ -39,7 +40,28 @@ public class TranslationService  implements ManagedService, RemoteService {
 
         this.executor = new ThreadPoolExecutor(threads, threads,
                 0L, TimeUnit.MILLISECONDS,
-                new PriorityBucketBlockingQueue<>(high_priority_queue_size, normal_priority_queue_size, background_priority_queue_size));
+                new PriorityBucketBlockingQueue<>(high_priority_queue_size, normal_priority_queue_size, background_priority_queue_size)) {
+
+            @Override
+            protected <T> RunnableFuture<T> newTaskFor(Runnable runnable, T value) {
+                RunnableFuture<T> task = super.newTaskFor(runnable, value);
+
+                if (runnable instanceof Prioritizable)
+                    task = new PriorityFuture<>(task, ((Prioritizable) runnable).getPriority());
+
+                return task;
+            }
+
+            @Override
+            protected <T> RunnableFuture<T> newTaskFor(Callable<T> callable) {
+                RunnableFuture<T> task = super.newTaskFor(callable);
+
+                if (callable instanceof Prioritizable)
+                    task = new PriorityFuture<>(task, ((Prioritizable) callable).getPriority());
+
+                return task;
+            }
+        };
     }
 
     ExecutorService getExecutor() {
