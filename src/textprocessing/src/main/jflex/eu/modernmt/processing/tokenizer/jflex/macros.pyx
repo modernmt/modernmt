c57b79e01d92037e5db2974509c295423859102f
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2018-02-08 18:36:18 +0100

    modified rule for handling full point in Cinese

--- src/textprocessing/src/main/jflex/eu/modernmt/processing/tokenizer/jflex/macros.pyx
+++ src/textprocessing/src/main/jflex/eu/modernmt/processing/tokenizer/jflex/macros.pyx
@@ -2,5 +2,5 @@ CJK        = [\u4E00-\u62FF\u6300-\u77FF\u7800-\u8CFF\u8D00-\u9FFF]
 _          = " "
 Letter     = !(![:letter:]|{CJK})
 
-UrlPart    = [[:letter:][:digit:]\-\_]+
+UrlPart    = ({Letter}|[[:digit:]\-\_])+
 Number     = ((\-|\+)?[0-9]+)?([\.,][0-9]+)*
