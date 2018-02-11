53c5a17303d7d175dc3afc8cf3b12e8907a62fb8
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2018-02-11 19:32:26 +0100

    changes to be compliant with the revised implementation of Chinese characters

--- src/textprocessing/src/main/java/eu/modernmt/cleaning/normalizers/chinese/ChineseDetector.java
+++ src/textprocessing/src/main/java/eu/modernmt/cleaning/normalizers/chinese/ChineseDetector.java
@@ -9,95 +9,76 @@ import eu.modernmt.lang.Language;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.HashSet;
+import java.util.PrimitiveIterator;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * ChineseDetector holds the vocabularies of Simplified/Traditional Chinese and variants for detecting the actual language of a text
  */
 public class ChineseDetector {
-    private static final Map<String, Set<Character>> dicts = new HashMap<>();
-    private static final Map<String, Float> priorities = new HashMap<>();
+    private static final Set<Integer> simplifiedVoc = new HashSet<>();
+    private static final Set<Integer> traditionalVoc = new HashSet<>();
 
     public ChineseDetector() {
-        if (dicts.size() == 0) {
-            loadDict();
+        // load all "characters" from the Simplified Chinese vocabulary
+        if (simplifiedVoc.size() == 0) {
+            loadDict(new Language("zh", "CN"), simplifiedVoc);
+        }
+
+        // load all "characters" from the Traditional Chinese vocabulary
+        if (traditionalVoc.size() == 0) {
+            loadDict(new Language("zh", "TW"), traditionalVoc);
+        }
+
+        // remove chars which are in both Simplified and Traditional Chinese vocabularies
+        if ( ( simplifiedVoc.size() > 0 ) && ( traditionalVoc.size() > 0 ) ) {
+            for (Object c : simplifiedVoc.toArray()) {
+                if (traditionalVoc.contains(c)) {
+                    simplifiedVoc.remove(c);
+                    traditionalVoc.remove(c);
+                }
+            }
         }
     }
 
     /**
      * load dictionary files into a map of Sets
      */
-    private void loadDict() {
-        dicts.clear();
-
-        Map<String, String> vocabularies = new HashMap<>();
+    private void loadDict(Language language, Set<Integer> voc) {
+        voc.clear();
+        String filename = language.getRegion() + ".voc";
         try {
             String xmlPath = getClass().getPackage().getName().replace('.', '/');
-            xmlPath = xmlPath + "/config/detector.json";
+            xmlPath = xmlPath + "/" + filename;
             URL url = getClass().getClassLoader().getResource(xmlPath);
+            if (url == null) {
+                throw new IOException("Vocabulary " + filename + " does not exist.");
+            }
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                String[] fields = inputLine.split(" = ");
-                vocabularies.put(fields[0], fields[1]);
+                PrimitiveIterator.OfInt stream = inputLine.codePoints().iterator();
+                while (stream.hasNext()) {
+                    int codepoint = stream.nextInt();
+                    voc.add(codepoint);
+                }
             }
             in.close();
-        } catch (MalformedURLException e) {
-            throw new Error(e);
-        } catch (IOException e) {
-            throw new Error(e);
-        }
 
-        try {
-            Float prior = 0.1f;
-            for (String _name : vocabularies.keySet()) {
-                String xmlPath = getClass().getPackage().getName().replace('.', '/');
-                xmlPath = xmlPath + "/vocabulary/" + vocabularies.get(_name);
-                URL url = getClass().getClassLoader().getResource(xmlPath);
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-
-                Set<Character> _set = new HashSet<>();
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    _set.add(inputLine.charAt(0));
-                }
-
-                dicts.put(_name, _set);
-                priorities.put(_name, prior);
-                prior = prior / 2;
-
-            }
-        } catch (MalformedURLException e) {
-            throw new Error(e);
         } catch (IOException e) {
             throw new Error(e);
         }
     }
 
     public void printStats() {
-        for (String _name : dicts.keySet()) {
-            System.out.println(_name + ": " + dicts.get(_name).size() + " symbols");
-        }
+        System.out.println("Simplified Chinese vocabulary : " + simplifiedVoc.size() + " symbols");
+        System.out.println("Traditional Chinese vocabulary: " + traditionalVoc.size() + " symbols");
     }
 
-    private Map<String, Float> computeScores(String string) {
-        char[] chars = string.toCharArray();
-        Map<String, Float> scores = new HashMap<>();
-
-        for (String _name : dicts.keySet()) {
-            scores.put(_name, priorities.get(_name));
-            scores.put(_name, 0.0f);
-            for (Character c : chars) {
-                if (dicts.get(_name).contains(c)) {
-                    scores.replace(_name, scores.get(_name), scores.get(_name) + 1);
-                }
-            }
-        }
-        return scores;
-    }
 
     /**
      * detect the language of the string
@@ -105,21 +86,18 @@ public class ChineseDetector {
      * @param string input string to convert
      * @return region
      */
-    public String detectRegion(String string) {
-        Map<String, Float> scores = computeScores(string);
-
-        String region = "";
-        Float best = -1000.1f;
-        for (String _name : scores.keySet()) {
-            Float s = scores.get(_name) + priorities.get(_name);
-            if (s > best) {
-                best = s;
-                region = _name;
-            }
+    private String detectRegion(String string) {
+        int simplifiedCount = 0, traditionalCount = 0;
+        PrimitiveIterator.OfInt stream = string.codePoints().iterator();
+        while (stream.hasNext()) {
+            int codepoint = stream.nextInt();
+            if (simplifiedVoc.contains(codepoint)) { simplifiedCount++; }
+            if (traditionalVoc.contains(codepoint)) { traditionalCount++; }
         }
-        return region;
+        if (simplifiedCount > traditionalCount) { return "CN"; }
+        else if (traditionalCount > 0) { return "TW"; }
+        else { return null; }
     }
-
 
     /**
      * detect the language of the string
@@ -131,37 +109,15 @@ public class ChineseDetector {
         return new Language("zh", detectRegion(string));
     }
 
-    public void printSupportedLanguages() {
-        for (String language : dicts.keySet()){
-            System.out.println("supported language:" + language);
-        }
-    }
-
-    public boolean support(Language language) {
-
-        if ( ! language.getLanguage().equals("zh")) { //language must be zh
-            return false;
-        }
-
-        String region = language.getRegion();
-        if (region == null) {
-            region = "TW"; //region is not specified; override with the defaultTraditional Chinese (TW)
-        }
-
-        if ( ! dicts.containsKey(region)) {
-            return false;
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         ChineseDetector detector = new ChineseDetector();
+        detector.printStats();
 
-        String[] strings = {"开放中文转换", "開放中文轉換", "偽", "香菸（英語：Cigarette", "為菸草製品的一種。滑鼠是一種很常見及", "常用的電腦輸入裝置"};
-
-        for (String string : strings) {
-            System.out.println("language:" + detector.detectLanguage(string) + " for string " + string);
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String inputLine = in.next();
+            System.out.print("language:" + detector.detectLanguage(inputLine) + " for string " + inputLine);
         }
     }
 }
+
