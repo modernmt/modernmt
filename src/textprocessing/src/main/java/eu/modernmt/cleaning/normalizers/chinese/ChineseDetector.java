62d63f3381854545c31b48bef2219f29d9e1108f
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2018-02-13 09:02:03 +0100

    code refactoring

--- src/textprocessing/src/main/java/eu/modernmt/cleaning/normalizers/chinese/ChineseDetector.java
+++ src/textprocessing/src/main/java/eu/modernmt/cleaning/normalizers/chinese/ChineseDetector.java
@@ -12,44 +12,47 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.PrimitiveIterator;
-import java.util.Scanner;
 import java.util.Set;
 
 /**
  * ChineseDetector holds the vocabularies of Simplified/Traditional Chinese and variants for detecting the actual language of a text
  */
 public class ChineseDetector {
-    private static final Set<Integer> simplifiedVoc = new HashSet<>();
-    private static final Set<Integer> traditionalVoc = new HashSet<>();
+    private static Set<Integer> simplifiedVoc = null;
+    private static Set<Integer> traditionalVoc = null;
 
-    public ChineseDetector() {
-        // load all "characters" from the Simplified Chinese vocabulary
-        if (simplifiedVoc.size() == 0) {
-            loadDict(new Language("zh", "CN"), simplifiedVoc);
-        }
+    private static ChineseDetector instance = null;
 
-        // load all "characters" from the Traditional Chinese vocabulary
-        if (traditionalVoc.size() == 0) {
-            loadDict(new Language("zh", "TW"), traditionalVoc);
-        }
-
-        // remove chars which are in both Simplified and Traditional Chinese vocabularies
-        if ( ( simplifiedVoc.size() > 0 ) && ( traditionalVoc.size() > 0 ) ) {
-            for (Object c : simplifiedVoc.toArray()) {
-                if (traditionalVoc.contains(c)) {
-                    simplifiedVoc.remove(c);
-                    traditionalVoc.remove(c);
-                }
+    public static ChineseDetector getInstance() throws IOException {
+        if (instance == null) {
+            synchronized (ChineseDetector.class) {
+                if (instance == null)
+                    instance = new ChineseDetector();
             }
+        }
+
+        return instance;
+    }
+
+    private ChineseDetector() throws IOException {
+        // load all Simplified-only Chinese character
+        if (simplifiedVoc == null) {
+            simplifiedVoc = loadDict("CN");
+        }
+
+        // load all Traditional-only Chinese character
+        if (traditionalVoc == null) {
+            traditionalVoc = loadDict("TW");
         }
     }
 
     /**
      * load dictionary files into a map of Sets
      */
-    private void loadDict(Language language, Set<Integer> voc) {
-        voc.clear();
-        String filename = language.getRegion() + ".voc";
+    private Set<Integer> loadDict(String region) throws IOException {
+        Set<Integer> voc = new HashSet<>();
+        String filename = region + ".voc";
+        BufferedReader in = null;
         try {
             String xmlPath = getClass().getPackage().getName().replace('.', '/');
             xmlPath = xmlPath + "/" + filename;
@@ -57,7 +60,7 @@ public class ChineseDetector {
             if (url == null) {
                 throw new IOException("Vocabulary " + filename + " does not exist.");
             }
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            in = new BufferedReader(new InputStreamReader(url.openStream()));
 
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
@@ -71,33 +74,19 @@ public class ChineseDetector {
 
         } catch (IOException e) {
             throw new Error(e);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
         }
+        return voc;
     }
 
     public void printStats() {
-        System.out.println("Simplified Chinese vocabulary : " + simplifiedVoc.size() + " symbols");
-        System.out.println("Traditional Chinese vocabulary: " + traditionalVoc.size() + " symbols");
+        System.err.println("Simplified-only Chinese vocabulary : " + simplifiedVoc.size() + " symbols");
+        System.err.println("Traditional-only Chinese vocabulary: " + traditionalVoc.size() + " symbols");
     }
 
-
-    /**
-     * detect the language of the string
-     *
-     * @param string input string to convert
-     * @return region
-     */
-    private String detectRegion(String string) {
-        int simplifiedCount = 0, traditionalCount = 0;
-        PrimitiveIterator.OfInt stream = string.codePoints().iterator();
-        while (stream.hasNext()) {
-            int codepoint = stream.nextInt();
-            if (simplifiedVoc.contains(codepoint)) { simplifiedCount++; }
-            if (traditionalVoc.contains(codepoint)) { traditionalCount++; }
-        }
-        if (simplifiedCount > traditionalCount) { return "CN"; }
-        else if (traditionalCount > 0) { return "TW"; }
-        else { return null; }
-    }
 
     /**
      * detect the language of the string
@@ -106,17 +95,56 @@ public class ChineseDetector {
      * @return language
      */
     public Language detectLanguage(String string) {
-        return new Language("zh", detectRegion(string));
+        int simplifiedCount = 0, traditionalCount = 0;
+        PrimitiveIterator.OfInt stream = string.codePoints().iterator();
+        while (stream.hasNext()) {
+            int codepoint = stream.nextInt();
+            if (simplifiedVoc.contains(codepoint)) {
+                simplifiedCount++;
+            }
+            if (traditionalVoc.contains(codepoint)) {
+                traditionalCount++;
+            }
+        }
+        if (simplifiedCount > traditionalCount) { return Language.CHINESE_SIMPLIFIED; }
+        else if (traditionalCount > 0) { return Language.CHINESE_TRADITIONAL;  }
+        else {return Language.CHINESE;  }
+    }
+
+    //    public Language detectLanguage(String string) {
+    public String scoreLanguage(String string) {
+        int simplifiedCount = 0, traditionalCount = 0;
+        PrimitiveIterator.OfInt stream = string.codePoints().iterator();
+        while (stream.hasNext()) {
+            int codepoint = stream.nextInt();
+            if (simplifiedVoc.contains(codepoint)) {
+                simplifiedCount++;
+            }
+            if (traditionalVoc.contains(codepoint)) {
+                traditionalCount++;
+            }
+        }
+        return simplifiedCount + " " + traditionalCount;
     }
 
     public static void main(String[] args) throws IOException {
-        ChineseDetector detector = new ChineseDetector();
+        ChineseDetector detector = ChineseDetector.getInstance();
         detector.printStats();
 
-        Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-            String inputLine = in.next();
-            System.out.print("language:" + detector.detectLanguage(inputLine) + " for string " + inputLine);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            String inputLine;
+            //loop forever an stream interruption is caught
+            //in case of empty line it returns an empty line
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(detector.detectLanguage(inputLine) + " " + inputLine);
+                System.out.println(detector.scoreLanguage(inputLine) + " " + inputLine);
+            }
+        } finally {
+            if (in != null){
+                in.close();
+            }
         }
     }
 }
