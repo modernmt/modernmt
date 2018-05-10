9f5f6e3e014c00f5b00f53b9d225ffbbb476ac44
Davide Caroselli <davide.caroselli@translated.net>
2018-05-10 16:57:38 -0700

    Small code refactoring

--- src/command-line/src/main/java/eu/modernmt/cli/AlignerFilterMain.java
+++ src/command-line/src/main/java/eu/modernmt/cli/AlignerFilterMain.java
@@ -17,10 +17,13 @@ import org.apache.commons.cli.*;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Level;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class AlignerFilterMain {
@@ -33,27 +36,27 @@ public class AlignerFilterMain {
             Option sourceLanguage = Option.builder("s").hasArg().required().build();
             Option targetLanguage = Option.builder("t").hasArg().required().build();
             Option inputPath = Option.builder().longOpt("input").hasArgs().required().build();
+            Option goldPath = Option.builder().longOpt("gold").hasArgs().required().build();
             Option outputPath = Option.builder().longOpt("output").hasArg().required().build();
             Option model = Option.builder().longOpt("model").hasArg().required().build();
-            Option threshold = Option.builder().longOpt("threshold").hasArg().required().build();
-            Option verbose = Option.builder().longOpt("verbose").hasArg(false).build();
+            Option verbose = Option.builder().longOpt("verbose").hasArg().required(false).build();
 
             cliOptions = new Options();
             cliOptions.addOption(sourceLanguage);
             cliOptions.addOption(targetLanguage);
             cliOptions.addOption(inputPath);
+            cliOptions.addOption(goldPath);
             cliOptions.addOption(outputPath);
             cliOptions.addOption(model);
-            cliOptions.addOption(threshold);
             cliOptions.addOption(verbose);
         }
 
         public final Language sourceLanguage;
         public final Language targetLanguage;
         public final File[] inputRoots;
+        public final File goldRoot;
         public final File outputRoot;
         public final File model;
-        public final float threshold;
         public final boolean verbose;
 
         public Args(String[] args) throws ParseException {
@@ -68,9 +71,9 @@ public class AlignerFilterMain {
             for (int i = 0; i < roots.length; i++)
                 inputRoots[i] = new File(roots[i]);
 
+            goldRoot = new File(cli.getOptionValue("gold"));
             outputRoot = new File(cli.getOptionValue("output"));
             model = new File(cli.getOptionValue("model"));
-            threshold = Float.parseFloat(cli.getOptionValue("threshold"));
             verbose = cli.hasOption("verbose");
         }
 
@@ -96,16 +99,22 @@ public class AlignerFilterMain {
         Preprocessor preprocessor = new Preprocessor();
 
         try {
+            ThresholdCalculator calculator = new ThresholdCalculator(args.sourceLanguage, args.targetLanguage, preprocessor, aligner, args.goldRoot);
+            float threshold = calculator.calculate();
+
+            System.err.println("Filtering with threshold: " + threshold);
+
             Filter filter = new Filter(language, aligner, preprocessor);
 
             for (MultilingualCorpus _corpus : bilingualCorpora) {
                 MultilingualCorpus corpus = new MultilingualCorpusMask(language, _corpus);
                 MultilingualCorpus output = new LazyWriterMultilingualCorpus(Corpora.rename(corpus, args.outputRoot));
 
-                filter.apply(corpus, output, args.threshold, args.verbose);
+                filter.apply(corpus, output, threshold, args.verbose);
             }
         } finally {
             IOUtils.closeQuietly(preprocessor);
+            IOUtils.closeQuietly(aligner);
         }
     }
 
@@ -225,4 +234,87 @@ public class AlignerFilterMain {
 
     }
 
+    private static class Counter implements Comparable<Counter> {
+
+        public final float threshold;
+        public int count = 0;
+
+        public Counter(float threshold) {
+            this.threshold = threshold;
+        }
+
+        @Override
+        public int compareTo(@NotNull Counter o) {
+            return Float.compare(threshold, o.threshold);
+        }
+    }
+
+    private static final class ThresholdCalculator {
+
+        private final LanguagePair language;
+        private final List<MultilingualCorpus> goldCorpora;
+        private final Preprocessor preprocessor;
+        private final FastAlign aligner;
+
+        public ThresholdCalculator(Language source, Language target, Preprocessor preprocessor, FastAlign aligner, File path) throws IOException {
+            this.language = new LanguagePair(source, target);
+            this.preprocessor = preprocessor;
+            this.aligner = aligner;
+
+            this.goldCorpora = new ArrayList<>();
+            Corpora.list(null, true, this.goldCorpora, source, target, path);
+        }
+
+        public float calculate() throws IOException {
+            HashMap<Float, Counter> counts = new HashMap<>();
+
+            for (MultilingualCorpus _corpus : goldCorpora) {
+                MultilingualCorpus corpus = new MultilingualCorpusMask(language, _corpus);
+                process(corpus, counts);
+            }
+
+            // Select threshold for 99%
+            ArrayList<Counter> array = new ArrayList<>(counts.values());
+            Collections.sort(array);
+            Collections.reverse(array);
+
+            double total = 0;
+            for (Counter c : array)
+                total += c.count;
+
+            double partial = 0;
+            for (Counter c : array) {
+                partial += c.count;
+                if (partial / total > 0.99)
+                    return c.threshold;
+            }
+
+            return array.get(array.size() - 1).threshold;
+        }
+
+        private void process(MultilingualCorpus corpus, HashMap<Float, Counter> counts) throws IOException {
+            MultilingualCorpus.MultilingualLineReader reader = null;
+
+            try {
+                reader = corpus.getContentReader();
+
+                MultilingualCorpus.StringPair pair;
+                while ((pair = reader.read()) != null) {
+                    Sentence source = preprocessor.process(language, pair.source);
+                    Sentence target = preprocessor.process(language.reversed(), pair.target);
+                    Alignment alignment = aligner.getAlignment(language, source, target);
+
+                    float score = alignment.getScore();
+                    score = ((int) (score * 2 + .5f)) / 2.f;
+
+                    counts.computeIfAbsent(score, Counter::new).count++;
+                }
+            } catch (ProcessingException | AlignerException e) {
+                throw new IOException(e);
+            } finally {
+                IOUtils.closeQuietly(reader);
+            }
+        }
+
+    }
 }
