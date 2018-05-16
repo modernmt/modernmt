90bfeb94bc39df027ab4e8bf004fd85d4e0f20ae
Davide Caroselli <davide.caroselli@translated.net>
2018-05-16 20:58:13 +0200

    Code refactoring

--- src/command-line/src/main/java/eu/modernmt/cli/AlignerScoreMain.java
+++ src/command-line/src/main/java/eu/modernmt/cli/AlignerScoreMain.java
@@ -26,7 +26,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class AlignerFilterMain {
+public class AlignerScoreMain {
 
     private static class Args {
 
@@ -36,28 +36,19 @@ public class AlignerFilterMain {
             Option sourceLanguage = Option.builder("s").hasArg().required().build();
             Option targetLanguage = Option.builder("t").hasArg().required().build();
             Option inputPath = Option.builder().longOpt("input").hasArgs().required().build();
-            Option goldPath = Option.builder().longOpt("gold").hasArgs().required().build();
-            Option outputPath = Option.builder().longOpt("output").hasArg().required().build();
             Option model = Option.builder().longOpt("model").hasArg().required().build();
-            Option verbose = Option.builder().longOpt("verbose").hasArg().required(false).build();
 
             cliOptions = new Options();
             cliOptions.addOption(sourceLanguage);
             cliOptions.addOption(targetLanguage);
             cliOptions.addOption(inputPath);
-            cliOptions.addOption(goldPath);
-            cliOptions.addOption(outputPath);
             cliOptions.addOption(model);
-            cliOptions.addOption(verbose);
         }
 
         public final Language sourceLanguage;
         public final Language targetLanguage;
-        public final File[] inputRoots;
-        public final File goldRoot;
-        public final File outputRoot;
+        public final File inputRoot;
         public final File model;
-        public final boolean verbose;
 
         public Args(String[] args) throws ParseException {
             CommandLineParser parser = new DefaultParser();
@@ -65,16 +56,8 @@ public class AlignerFilterMain {
 
             sourceLanguage = Language.fromString(cli.getOptionValue('s'));
             targetLanguage = Language.fromString(cli.getOptionValue('t'));
-
-            String[] roots = cli.getOptionValues("input");
-            inputRoots = new File[roots.length];
-            for (int i = 0; i < roots.length; i++)
-                inputRoots[i] = new File(roots[i]);
-
-            goldRoot = new File(cli.getOptionValue("gold"));
-            outputRoot = new File(cli.getOptionValue("output"));
+            inputRoot = new File(cli.getOptionValue("input"));
             model = new File(cli.getOptionValue("model"));
-            verbose = cli.hasOption("verbose");
         }
 
     }
@@ -85,153 +68,23 @@ public class AlignerFilterMain {
         Args args = new Args(_args);
 
         ArrayList<MultilingualCorpus> bilingualCorpora = new ArrayList<>();
-        Corpora.list(null, true, bilingualCorpora, args.sourceLanguage, args.targetLanguage, args.inputRoots);
+        Corpora.list(null, true, bilingualCorpora, args.sourceLanguage, args.targetLanguage, args.inputRoot);
 
         if (bilingualCorpora.isEmpty())
             throw new ParseException("Input path does not contains valid bilingual data");
-
-        FileUtils.deleteDirectory(args.outputRoot);
-        FileUtils.forceMkdir(args.outputRoot);
-
-        LanguagePair language = new LanguagePair(args.sourceLanguage, args.targetLanguage);
 
         FastAlign aligner = new FastAlign(args.model);
         Preprocessor preprocessor = new Preprocessor();
 
         try {
-            ThresholdCalculator calculator = new ThresholdCalculator(args.sourceLanguage, args.targetLanguage, preprocessor, aligner, args.goldRoot);
+            ThresholdCalculator calculator = new ThresholdCalculator(args.sourceLanguage, args.targetLanguage, preprocessor, aligner, args.inputRoot);
             float threshold = calculator.calculate();
 
-            System.err.println("Filtering with threshold: " + threshold);
-
-            Filter filter = new Filter(language, aligner, preprocessor);
-
-            for (MultilingualCorpus _corpus : bilingualCorpora) {
-                MultilingualCorpus corpus = new MultilingualCorpusMask(language, _corpus);
-                MultilingualCorpus output = new LazyWriterMultilingualCorpus(Corpora.rename(corpus, args.outputRoot));
-
-                filter.apply(corpus, output, threshold, args.verbose);
-            }
+            System.out.println(Float.toString(threshold));
         } finally {
             IOUtils.closeQuietly(preprocessor);
             IOUtils.closeQuietly(aligner);
         }
-    }
-
-    private static final class Filter {
-
-        private final LanguagePair language;
-        private final FastAlign aligner;
-        private final Preprocessor preprocessor;
-
-        public Filter(LanguagePair language, FastAlign aligner, Preprocessor preprocessor) {
-            this.language = language;
-            this.aligner = aligner;
-            this.preprocessor = preprocessor;
-        }
-
-        public void apply(MultilingualCorpus corpus, MultilingualCorpus output, float threshold, boolean verbose) throws IOException {
-            Batch batch = new Batch();
-
-            MultilingualCorpus.MultilingualLineReader reader = null;
-            MultilingualCorpus.MultilingualLineWriter writer = null;
-
-            try {
-                reader = corpus.getContentReader();
-                writer = output.getContentWriter(false);
-
-                MultilingualCorpus.StringPair pair;
-                while ((pair = reader.read()) != null) {
-                    while (!batch.add(pair)) {
-                        processBatch(batch, threshold, writer, verbose);
-                        batch.clear();
-                    }
-                }
-
-                if (batch.size() > 0) {
-                    processBatch(batch, threshold, writer, verbose);
-                    batch.clear();
-                }
-            } catch (ProcessingException | AlignerException e) {
-                throw new IOException(e);
-            } finally {
-                IOUtils.closeQuietly(reader);
-                IOUtils.closeQuietly(writer);
-            }
-        }
-
-        private void processBatch(Batch batch, float threshold, MultilingualCorpus.MultilingualLineWriter writer, boolean verbose)
-                throws ProcessingException, AlignerException, IOException {
-            List<Sentence> sources = preprocessor.process(language, batch.getSources());
-            List<Sentence> targets = preprocessor.process(language.reversed(), batch.getTargets());
-            Alignment[] alignments = aligner.getAlignments(language, sources, targets);
-
-            int i = 0;
-            for (MultilingualCorpus.StringPair pair : batch.getPairs()) {
-                if (alignments[i].getScore() >= threshold) {
-                    writer.write(pair);
-                } else {
-                    if (verbose)
-                        System.err.println(pair);
-                }
-
-                i++;
-            }
-        }
-
-    }
-
-    private static final class Batch {
-
-        private final int maxSize;
-        private final ArrayList<String> sources;
-        private final ArrayList<String> targets;
-        private final ArrayList<MultilingualCorpus.StringPair> pairs;
-
-        public Batch() {
-            this(Runtime.getRuntime().availableProcessors() * 2);
-        }
-
-        public Batch(int size) {
-            this.maxSize = size;
-            this.sources = new ArrayList<>(size);
-            this.targets = new ArrayList<>(size);
-            this.pairs = new ArrayList<>(size);
-        }
-
-        public boolean add(MultilingualCorpus.StringPair pair) {
-            if (sources.size() >= maxSize)
-                return false;
-
-            sources.add(pair.source);
-            targets.add(pair.target);
-            pairs.add(pair);
-
-            return true;
-        }
-
-        public void clear() {
-            sources.clear();
-            targets.clear();
-            pairs.clear();
-        }
-
-        public int size() {
-            return sources.size();
-        }
-
-        public ArrayList<MultilingualCorpus.StringPair> getPairs() {
-            return pairs;
-        }
-
-        public ArrayList<String> getSources() {
-            return sources;
-        }
-
-        public ArrayList<String> getTargets() {
-            return targets;
-        }
-
     }
 
     private static class Counter implements Comparable<Counter> {
