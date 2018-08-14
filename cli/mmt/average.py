fixed computation of average checkpoint; code cleanup;

cli/mmt/average.py
@@ -5,9 +5,9 @@ import sys
 import tensorflow as tf
 import logging
 import numpy as np
+import six
 
-
-def finalize_model(str_checkpoints, model_dir, n_checkpoints=None, gpus=None):
+def finalize_model(str_checkpoints, output_dir, n_checkpoints=None, gpus=None):
 
     logging.basicConfig(format='%(asctime)-15s [%(levelname)s] - %(message)s',
                         level=logging.DEBUG)
@@ -20,6 +20,7 @@ def finalize_model(str_checkpoints, model_dir, n_checkpoints=None, gpus=None):
     print "checkpoints:%s" % (checkpoints)
     # Read variables from all checkpoints and average them.
     var_list = tf.contrib.framework.list_variables(checkpoints[0])
+    var_dtypes = {}
     var_values = {name: np.zeros(shape) for name, shape in var_list if not name.startswith("global_step")}
 
     for checkpoint in checkpoints:
@@ -27,32 +28,47 @@ def finalize_model(str_checkpoints, model_dir, n_checkpoints=None, gpus=None):
 
         for name in var_values:
             tensor = reader.get_tensor(name)
+            var_dtypes[name] = tensor.dtype
             var_values[name] += tensor
+        tf.logging.info("Read from checkpoint %s", checkpoint)
 
     for name in var_values:  # Average.
         var_values[name] /= len(checkpoints)
 
-    device = '/cpu:0'
-    if gpus is not None:
-        print "finalize_model gpus:%s" % (gpus)
-        print "finalize_model gpus.split(','):%s" % (gpus.split(','))
-        print "finalize_model gpus.split(',')[0]:%s" % (gpus.split(',')[0])
+    if gpus is not None: # if gpu computation is allowed use the first
+        gpu = gpus.split(',')[0]
+        device = '/device:GPU:%s' % (gpu)
+    else:
+        gpu = None
+        device = '/cpu:0'
 
-        device='/device:GPU:%s' % (gpus.split(',')[0])
-
-
-    print "finalize_model device:%s" % (device)
     with tf.device(device):
-    # with tf.device('/device:GPU:%d' % gpus.split(',')[0] if gpus is not None else '/cpu:0'):
-        tf_vars = [tf.get_variable(name, shape=var.shape, dtype=var.dtype) for name, var in var_values.iteritems()]
+        tf_vars = [ tf.get_variable(v, shape=var_values[v].shape, dtype=var_dtypes[name]) for v in var_values ]
         placeholders = [tf.placeholder(v.dtype, shape=v.shape) for v in tf_vars]
         assign_ops = [tf.assign(v, p) for (v, p) in zip(tf_vars, placeholders)]
-        tf.Variable(0, name="global_step", trainable=False, dtype=tf.int64)
-    tf_vars = [tf.get_variable(name, shape=var.shape, dtype=var.dtype) for name, var in var_values.iteritems()]
-    placeholders = [tf.placeholder(v.dtype, shape=v.shape) for v in tf_vars]
-    assign_ops = [tf.assign(v, p) for (v, p) in zip(tf_vars, placeholders)]
-    tf.Variable(0, name="global_step", trainable=False, dtype=tf.int64)
+        global_step = tf.Variable(0, name="global_step", trainable=False, dtype=tf.int64)
     saver = tf.train.Saver(tf.global_variables(), save_relative_paths=True)
+
+    # Build a model consting only of variables, set them to the average values.
+    # model_output_path = output_dir
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    session_config = tf.ConfigProto(allow_soft_placement=True)
+    session_config.gpu_options.allow_growth = True
+    if gpu is not None:
+        session_config.gpu_options.force_gpu_compatible = True
+        session_config.gpu_options.visible_device_list = str(gpu)
+
+    sess = tf.Session(config=session_config)
+    sess.run(tf.initialize_all_variables())
+    for p, assign_op, (name, value) in zip(placeholders, assign_ops, six.iteritems(var_values)):
+        sess.run(assign_op, {p: value})
+    saver.save(sess, os.path.join(output_dir, 'model-avg'), global_step=global_step)
+
+    # Use the built saver to save the averaged checkpoint.
+    tf.logging.info("Averaged checkpoints saved in %s", output_dir)
+
 
 
 def main(argv):

cli/mmt/engine.py
@@ -229,10 +229,11 @@ class NeuralDecoder(object):
 
         global_steps = max([(steps - (steps % 100)) for steps, _ in checkpoint_pairs])
         checkpoints = [path for (_, path) in checkpoint_pairs]
-        logger.log(logging.INFO, "checkpoints:%s" % (checkpoints))
+        logger.log(logging.INFO, "average on these checkpoints:%s" % (checkpoints))
 
         # Read variables from all checkpoints and average them.
         var_list = tf.contrib.framework.list_variables(checkpoints[0])
+        var_dtypes = {}
         var_values = {name: np.zeros(shape) for name, shape in var_list if not name.startswith("global_step")}
 
         for checkpoint in checkpoints:
@@ -240,21 +241,21 @@ class NeuralDecoder(object):
 
             for name in var_values:
                 tensor = reader.get_tensor(name)
+                var_dtypes[name] = tensor.dtype
                 var_values[name] += tensor
 
         for name in var_values:  # Average.
             var_values[name] /= len(checkpoints)
 
         if gpus is not None:
-            gpu = gpus.split(',')[0]
+            gpu = gpus[0]
             device = '/device:GPU:%s' % (gpu)
         else:
             gpu = None
             device = '/cpu:0'
 
-        logger.log(logging.INFO, "finalize_model device:%s" % (device))
         with tf.device(device):
-            tf_vars = [tf.get_variable(name, shape=var.shape, dtype=var.dtype) for name, var in var_values.iteritems()]
+            tf_vars = [ tf.get_variable(v, shape=var_values[v].shape, dtype=var_dtypes[name]) for v in var_values ]
             placeholders = [tf.placeholder(v.dtype, shape=v.shape) for v in tf_vars]
             assign_ops = [tf.assign(v, p) for (v, p) in zip(tf_vars, placeholders)]
             tf.Variable(0, name="global_step", trainable=False, dtype=tf.int64)
@@ -277,6 +278,8 @@ class NeuralDecoder(object):
 
         # with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
+        for p, assign_op, (name, value) in zip(placeholders, assign_ops, six.iteritems(var_values)):
+            sess.run(assign_op, {p: value})
 
         # Use the built saver to save the averaged checkpoint.
         saver.save(sess, os.path.join(model_output_path, 'model-avg'), global_step=global_steps)
@@ -765,4 +768,4 @@ class EngineBuilder:
     @Step(7, 'Pack model')
     def _pack_model(self, args, skip=False):
         if not skip:
-            self._decoder.finalize_model(args.prepared_data_path, args.train_model_path, self._gpus)
+            self._decoder.finalize_model(args.prepared_data_path, args.train_model_path, gpus=self._gpus)

src/decoder-neural/src/main/python/nmmt/transformer.py
@@ -243,13 +243,16 @@ class TransformerDecoder(object):
             raw_output = output_text
 
         # align
-        results = self._session.run(self._attention_mats_op, {
-            self._ph_infer_inputs: inputs,
-            self._ph_train_inputs: np.reshape(inputs, [1, -1, 1, 1]),
-            self._ph_train_targets: np.reshape(outputs, [1, -1, 1, 1]),
-        })
+        if len(outputs) > 0:      # if output is empty the forced decoding does not work; reshape of an empty array is not possible
+            results = self._session.run(self._attention_mats_op, {
+                self._ph_infer_inputs: inputs,
+                self._ph_train_inputs: np.reshape(inputs, [1, -1, 1, 1]),
+                self._ph_train_targets: np.reshape(outputs, [1, -1, 1, 1]),
+            })
 
-        alignment = self._make_alignment(input_indexes, output_indexes, results)
+            alignment = self._make_alignment(input_indexes, output_indexes, results)
+        else:
+            alignment = []
 
         return Translation(text=raw_output, alignment=alignment)
 