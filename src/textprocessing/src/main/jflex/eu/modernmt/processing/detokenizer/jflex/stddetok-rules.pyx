b1ff6723fc1b782454df68eb01f9416ae2e55370
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2018-09-18 08:36:05 +0200

    changes to improve German detokenization

--- src/textprocessing/src/main/jflex/eu/modernmt/processing/detokenizer/jflex/stddetok-rules.pyx
+++ src/textprocessing/src/main/jflex/eu/modernmt/processing/detokenizer/jflex/stddetok-rules.pyx
@@ -2,7 +2,7 @@
 .                                                                   { /* ignore */ }
 
 {_}[\(\[\{\¿\¡]+{_}                                                 { return REMOVE_LAST; }
-{_}[\,\.\?\!\:\;\\\%\}\]\)]+{_}                                     { return REMOVE_FIRST; }
+{_}[\,\.\?\!\:\;\\\}\]\)]+{_}                                     { return REMOVE_FIRST; }
 
 {_}{quot}{_}                                                        { return this.quoteCount++ % 2 == 0 ? REMOVE_LAST : REMOVE_FIRST; }
 {_}{apos}{_}                                                        { return this.aposCount++ % 2 == 0 ? REMOVE_LAST : REMOVE_FIRST; }
@@ -11,4 +11,4 @@
 {_}\/{_}                                                            { return REMOVE_ALL; }
 […\.[:letter:][:digit:]]{_}…{_}                                     { yypushback(1); return REMOVE_FIRST; }
 
-{_}[\$\%]{_}\{[a-zA-Z0-9_ ]+{_}\}{_}                                { return REMOVE_INNER; }
+{_}[\$]{_}\{[a-zA-Z0-9_ ]+{_}\}{_}                                { return REMOVE_INNER; }
