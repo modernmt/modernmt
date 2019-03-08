c165dfae43a72fcf8a468c6e93c9f3495c20b97e
Davide Caroselli <davide@modernmt.eu>
2019-03-08 19:09:00 +0100

    Implemented experimental chinese tokenizer

--- src/textprocessing/src/main/java/eu/modernmt/processing/tokenizer/impl/ChineseTokenizer.java
+++ src/textprocessing/src/main/java/eu/modernmt/processing/tokenizer/impl/ChineseTokenizer.java
@@ -3,6 +3,7 @@ package eu.modernmt.processing.tokenizer.impl;
 import eu.modernmt.lang.Language;
 import eu.modernmt.lang.UnsupportedLanguageException;
 import eu.modernmt.processing.tokenizer.BaseTokenizer;
+import eu.modernmt.processing.tokenizer.StatisticalChineseAnnotator;
 import eu.modernmt.processing.tokenizer.jflex.annotators.CommonTermsTokenAnnotator;
 
 import java.io.Reader;
@@ -11,7 +12,8 @@ public class ChineseTokenizer extends BaseTokenizer {
 
     public ChineseTokenizer(Language sourceLanguage, Language targetLanguage) throws UnsupportedLanguageException {
         super(sourceLanguage, targetLanguage);
-        
+
+        super.annotators.add(new StatisticalChineseAnnotator());
         super.annotators.add(new CommonTermsTokenAnnotator((Reader) null));
     }
 }
