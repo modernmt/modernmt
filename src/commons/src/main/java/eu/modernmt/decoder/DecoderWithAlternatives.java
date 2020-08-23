af73c7ffe500d6ab1a6edd2d240f2caf1716fdf0
nicolabertoldi <nicola@modernmt.eu>
2020-08-23 17:50:43 +0200

    changed member name: from 'nbest' into 'alternatives'

---
+++ src/commons/src/main/java/eu/modernmt/decoder/DecoderWithAlternatives.java
@@ -0,0 +1,20 @@
+package eu.modernmt.decoder;
+
+import eu.modernmt.lang.LanguageDirection;
+import eu.modernmt.model.ContextVector;
+import eu.modernmt.model.Priority;
+import eu.modernmt.model.Sentence;
+import eu.modernmt.model.Translation;
+
+import java.util.UUID;
+
+/**
+ * Created by davide on 23/05/17.
+ */
+public interface DecoderWithAlternatives {
+
+    Translation translate(Priority priority, UUID user, LanguageDirection direction, Sentence text, int nbestListSize, long expiration) throws DecoderException;
+
+    Translation translate(Priority priority, UUID user, LanguageDirection direction, Sentence text, ContextVector contextVector, int nbestListSize, long expiration) throws DecoderException;
+
+}
