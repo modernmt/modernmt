329caa698736daa30584546846fa644753e86b30
nicolabertoldi <nicola@modernmt.eu>
2020-10-20 19:04:10 +0200

    code cleanup

--- src/textprocessing/src/main/java/eu/modernmt/processing/string/SentenceCompiler.java
+++ src/textprocessing/src/main/java/eu/modernmt/processing/string/SentenceCompiler.java
@@ -2,7 +2,6 @@ package eu.modernmt.processing.string;
 
 import eu.modernmt.model.Sentence;
 import eu.modernmt.model.Tag;
-import eu.modernmt.processing.ProcessingException;
 import eu.modernmt.processing.TextProcessor;
 import eu.modernmt.processing.tags.format.InputFormat;
 import org.apache.logging.log4j.LogManager;
